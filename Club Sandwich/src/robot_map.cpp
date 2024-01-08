fdb9b5ea9484e6d4f008a061e44dba4c40cad451
Tristan Warder <tristan.warder@outlook.com>
2024-01-08 17:44:52 -0500

    Inverse motor

--- Club Sandwich/src/robot_map.cpp
+++ Club Sandwich/src/robot_map.cpp
@@ -6,7 +6,7 @@ Motor driveBLeft{1, MOTOR_GEAR_BLUE, false, MOTOR_ENCODER_DEGREES}; //1
 Motor driveCLeft{2, MOTOR_GEAR_BLUE, false, MOTOR_ENCODER_DEGREES}; //2
 Motor driveFLeft{3, MOTOR_GEAR_BLUE, false, MOTOR_ENCODER_DEGREES}; //3
 Motor driveBRight{11, MOTOR_GEAR_BLUE, true, MOTOR_ENCODER_DEGREES}; //11
-Motor driveCRight{12, MOTOR_GEAR_BLUE, false, MOTOR_ENCODER_DEGREES}; //12
+Motor driveCRight{12, MOTOR_GEAR_BLUE, true, MOTOR_ENCODER_DEGREES}; //12
 Motor driveFRight{13, MOTOR_GEAR_BLUE, true, MOTOR_ENCODER_DEGREES}; //13
 
 //Flywheel
